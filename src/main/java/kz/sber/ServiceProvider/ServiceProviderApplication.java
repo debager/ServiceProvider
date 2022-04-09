fc5aaf9b878488444278b925bf8cc7e67baa7b97
debager <65409178+debager@users.noreply.github.com>
2022-04-09 21:43:11 +0700

    Initial commit

---
+++ src/main/java/kz/sber/ServiceProvider/ServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package kz.sber.ServiceProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProviderApplication.class, args);
+	}
+
+}
